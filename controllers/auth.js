c00d0304d06e07037ee4b7f2dbcf4f7d251198e7
Madhumitha <madhumithavenu03@gmail.com>
2023-10-06 12:31:26 +0530

    Alert for wrong password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,35 @@
-exports.register = (req, res)=>{
+const mysql = require('mysql');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const async = require('hbs/lib/async');
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+})
+
+exports.register = (req, res) => {
     console.log(req.body);
-    res.json("Form Submited")
+
+    const { name, email, password, passwordConfirm } = req.body;
+    db.query('SELECT email FROM users WHERE email=?', [email], async (err, results)=>{
+        if(err){
+            console.log(err);
+        }
+        if(results.length>0){
+            return res.render('register', {
+                message: 'That email is already in use'
+            })
+        }else if(password !== passwordConfirm){
+            return res.render('register', {
+                message: 'Password do not match'
+            })
+        }
+
+        let hashedPassword = await bcrypt.hash(password, 8);
+        console.log(hashedPassword);
+    });
+    
 }
