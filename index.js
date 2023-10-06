4dfb58f09c0414d57d5d226a3ff7f1113d370235
Madhumitha <madhumithavenu03@gmail.com>
2023-10-06 10:17:08 +0530

    starting the server

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get("/", (req,res)=>{
+    res.send("<h1>Home Page</h1>")
+})
+
+app.listen(5000, ()=>{
+    console.log("Server Started on port 5000");
+})
