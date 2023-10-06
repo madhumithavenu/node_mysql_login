b1a096e141dbb28071a9024f4b0cd0429f64d5fa
Madhumitha <madhumithavenu03@gmail.com>
2023-10-06 11:00:56 +0530

    Adding public and views

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 const express = require('express');
+const path = require('path');
 const mysql = require('mysql');
 const dotenv = require('dotenv');
-dotenv.config({ path: './.env'});
+
+dotenv.config({ path: './.env' });
+
 const app = express();
+
 const db = mysql.createConnection({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE
 })
-db.connect((err)=>{
-    if(err){
+const publicDirectory = path.join(__dirname, './public')
+
+app.use(express.static(publicDirectory))
+app.set('view engine', 'hbs');
+
+db.connect((err) => {
+    if (err) {
         console.log();
-    }else{
+    } else {
         console.log("MySQL Connected");
     }
 })
 
-app.get("/", (req,res)=>{
-    res.send("<h1>Home Page</h1>")
+app.get("/", (req, res) => {
+    // res.send("<h1>Home Page</h1>")
+    res.render("index");
 })
 
-app.listen(5000, ()=>{
+app.listen(5000, () => {
     console.log("Server Started on port 5000");
 })
