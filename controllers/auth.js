135bc0d6c98ee03e07a52de1b02bc54785f8b46f
Madhumitha <madhumithavenu03@gmail.com>
2023-10-06 12:46:26 +0530

    User registered successfully

--- controllers/auth.js
+++ controllers/auth.js
@@ -14,15 +14,15 @@ exports.register = (req, res) => {
     console.log(req.body);
 
     const { name, email, password, passwordConfirm } = req.body;
-    db.query('SELECT email FROM users WHERE email=?', [email], async (err, results)=>{
-        if(err){
+    db.query('SELECT email FROM users WHERE email=?', [email], async (err, results) => {
+        if (err) {
             console.log(err);
         }
-        if(results.length>0){
+        if (results.length > 0) {
             return res.render('register', {
                 message: 'That email is already in use'
             })
-        }else if(password !== passwordConfirm){
+        } else if (password !== passwordConfirm) {
             return res.render('register', {
                 message: 'Password do not match'
             })
@@ -30,6 +30,18 @@ exports.register = (req, res) => {
 
         let hashedPassword = await bcrypt.hash(password, 8);
         console.log(hashedPassword);
+
+        // res.send("testing");
+        db.query('INSERT INTO users SET ?', { name: name, email: email, password: hashedPassword }, (err, results)=>{
+            if(err){
+                console.log(err);
+            }else{
+                console.log(results);
+                return res.render('register',{
+                    message: 'User registered'
+                })
+            }
+        })
     });
-    
+
 }
